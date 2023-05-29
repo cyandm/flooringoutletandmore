1f061195ea948049581d2ad1513be74f9263074e
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2023-05-29 12:02:44 +0330

    sidebar-blog create

---
+++ single-post.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+
+<main class="single-post">
+	<?php get_template_part( './templates/sidebar', 'blog' ); ?>
+
+</main>
+<?php get_footer() ?>
---
+++ templates/sidebar-blog.php
@@ -0,0 +1,91 @@
+<aside class="side-bar blog">
+	<div class="box">
+		<div class="search">
+			<form id="homeContactForm" action="/" method="get">
+				<div class="form_control">
+					<input type="text" name="s" id="search" value="<?php the_search_query(); ?>"
+						placeholder="Quick Search" />
+
+					<i class="icon-Component-2-1"></i>
+				</div>
+			</form>
+		</div>
+		<div class="categories">
+			<span class="h4">Categories</span>
+			<ul>
+				<li class="active">
+					<span>All</span>
+					<span class="caption">215</span>
+				</li>
+				<li>
+					<span>All</span>
+					<span class="caption">215</span>
+				</li>
+				<li>
+					<span>All</span>
+					<span class="caption">215</span>
+				</li>
+				<li>
+					<span>All</span>
+					<span class="caption">215</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="box">
+		<span class="h4">Recommended</span>
+
+		<article>
+			<div class="img-wrapper">
+				<img src="<?php echo get_stylesheet_directory_uri() . '/imgs/blog-1.png' ?>" alt="">
+			</div>
+			<div class="content">
+				<span class="h5">Ways To Decide Ways To Decide</span>
+				<span class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus hic
+					nostrum, fugiat deleniti a iusto ipsum nesciunt perspiciatis ullam eligendi obcaecati
+					consequuntur reprehenderit natus modi!</span>
+				<span class="caption">2023/05/23</span>
+			</div>
+		</article>
+
+		<article>
+			<div class="img-wrapper">
+				<img src="<?php echo get_stylesheet_directory_uri() . '/imgs/blog-1.png' ?>" alt="">
+			</div>
+			<div class="content">
+				<span class="h5">Ways To Decide Ways To Decide</span>
+				<span class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus hic
+					nostrum, fugiat deleniti a iusto ipsum nesciunt perspiciatis ullam eligendi obcaecati
+					consequuntur reprehenderit natus modi!</span>
+				<span class="caption">2023/05/23</span>
+			</div>
+		</article>
+
+		<article>
+			<div class="img-wrapper">
+				<img src="<?php echo get_stylesheet_directory_uri() . '/imgs/blog-1.png' ?>" alt="">
+			</div>
+			<div class="content">
+				<span class="h5">Ways To Decide Ways To Decide</span>
+				<span class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus hic
+					nostrum, fugiat deleniti a iusto ipsum nesciunt perspiciatis ullam eligendi obcaecati
+					consequuntur reprehenderit natus modi!</span>
+				<span class="caption">2023/05/23</span>
+			</div>
+		</article>
+
+		<article>
+			<div class="img-wrapper">
+				<img src="<?php echo get_stylesheet_directory_uri() . '/imgs/blog-1.png' ?>" alt="">
+			</div>
+			<div class="content">
+				<span class="h5">Ways To Decide Ways To Decide</span>
+				<span class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus hic
+					nostrum, fugiat deleniti a iusto ipsum nesciunt perspiciatis ullam eligendi obcaecati
+					consequuntur reprehenderit natus modi!</span>
+				<span class="caption">2023/05/23</span>
+			</div>
+		</article>
+	</div>
+
+</aside>
