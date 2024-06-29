b615e3604dfed7655f9891d77c154949aaf63dae
amirali-dizabadi <amirali.dizabadi@gmail.com>
2024-06-29 12:47:00 +0330

    fix close past submit popup

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,47 @@
 function getCookie(cname) {
-	let name = cname + '=';
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return '';
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 function specialPopup() {
-	const popUp = document.querySelector('#popUp');
-	const popupClose = document.querySelector('.popupClose');
+  const popUp = document.querySelector("#popUp");
+  const popUpBTn = document.querySelector("#popUp #contact-us-form-submit");
+  const popupClose = document.querySelector(".popupClose");
 
-	if (!popUp || !popupClose) return;
+  if (!popUp || !popupClose) return;
 
-	if (getCookie('specialPopup') === 'true') return;
+  if (getCookie("specialPopup") === "true") return;
 
-	setTimeout(() => {
-		popUp.classList.add('active');
-	}, 3000);
+  setTimeout(() => {
+    popUp.classList.add("active");
+  }, 3000);
 
-	popupClose.addEventListener('click', () => {
-		popUp.classList.remove('active');
+  popupClose.addEventListener("click", () => {
+    popUp.classList.remove("active");
 
-		//tomorrow
-		const now = new Date();
-		now.setDate(now.getDate() + 1);
+    //tomorrow
+    const now = new Date();
+    now.setDate(now.getDate() + 1);
 
-		document.cookie = 'specialPopup=true; expires=' + now.toUTCString();
-	});
+    document.cookie = "specialPopup=true; expires=" + now.toUTCString();
+  });
+
+  popUpBTn.addEventListener("click", () => {
+    setTimeout(() => {
+      popUp.classList.remove("active");
+    }, 700);
+  });
 }
 
 specialPopup();
--- templates/special-popup.php
+++ templates/special-popup.php
@@ -1,5 +1,4 @@
-<div class="popup-back"
-	 id="popUp">
+<div class="popup-back" id="popUp">
 
 	<div class="popupClose">
 		<i class="icon-close"></i>
@@ -9,37 +8,20 @@
 		<span class="h2">
 			What are you looking for?
 		</span>
-		<form id="contact-us-form"
-			  action="#"
-			  method='post'>
+		<form id="contact-us-form" action="#" method='post'>
 			<label for="email">
 				Email
-				<input class="form_control"
-					   type="email"
-					   name="email"
-					   id="email"
-					   placeholder="email"
-					   required>
+				<input class="form_control" type="email" name="email" id="email" placeholder="email" required>
 			</label>
 
 			<label for="phone-number">
 				Phone number
-				<input type="tel"
-					   class="form_control"
-					   name="phone-number"
-					   id="phone-number"
-					   placeholder="Phone number"
-					   required>
+				<input type="tel" class="form_control" name="phone-number" id="phone-number" placeholder="Phone number" required>
 			</label>
 
 			<label for="describe">
 				what are you looking for
-				<textarea name="describe"
-						  class="form_control"
-						  id="describe"
-						  cols=" 30"
-						  rows="10"
-						  placeholder="Describe"></textarea>
+				<textarea name="describe" class="form_control" id="describe" cols=" 30" rows="10" placeholder="Describe"></textarea>
 			</label>
 
 			<!-- <label for="agreement"
@@ -52,9 +34,9 @@
 				I want you to inform me about new products and new offers
 			</label> -->
 
-			<button type="submit"
-					id="contact-us-form-submit"
-					class="btn bg_secondary1">
+			<div class="cf-turnstile" data-sitekey="0x4AAAAAAAduRsbVAHyEFDb5" data-callback="javascriptCallback"></div>
+
+			<button type="submit" id="contact-us-form-submit" class="btn bg_secondary1">
 				send
 				<i class="icon-Component-2-1"></i>
 			</button>
